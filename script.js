5cb0fb828f685fa84eb054259c04f6cbfe5b66bd
AMarikDS <167884336+AMarikDS@users.noreply.github.com>
2025-02-10 11:42:08 +0300

    Feat: update personal info

--- script.js
+++ script.js
@@ -5,20 +5,43 @@ function toggleDarkMode() {
 
 // Opens the resume PDF in a new browser tab
 function openResume() {
-  window.open("https://pdfobject.com/pdf/sample.pdf", "_blank");
+  window.open("assets/AMarikDS-CV.pdf", "_blank");
 }
 
 // Array containing badge data with icon names and labels
 // Icons can be referenced from https://icon-sets.iconify.design/
 const badgesData = [
-  { icon: "devicon:html5", label: "HTML" },
-  { icon: "devicon:css3", label: "CSS" },
-  { icon: "devicon:javascript", label: "JavaScript" },
-  { icon: "devicon:figma", label: "Figma" },
-  { icon: "devicon:json", label: "JSON" },
+  { icon: "devicon:python", label: "Python" },
+  { icon: "devicon:pandas", label: "Pandas" },
+  { icon: "devicon:numpy", label: "NumPy" },
+  { icon: "logos:seaborn-icon", label: "Seaborn" },
+  { icon: "devicon:opencv", label: "OpenCV" },
+  { icon: "devicon:scikitlearn", label: "Scikit-learn" },
+  { icon: "devicon:jupyter", label: "Jupyter" },
+  { icon: "devicon:tensorflow", label: "TensorFlow" },
+  { icon: "devicon:keras", label: "Keras" },
+  { icon: "devicon:googlecolab", label: "Google Colab" },
+  { icon: "logos:hugging-face-icon", label: "Hugging Face" },
+  { icon: "devicon:pytorch", label: "PyTorch" },
+  { icon: "devicon:fastapi", label: "FastAPI" },
+  { icon: "devicon:openapi", label: "OpenAPI" },
+  { icon: "devicon:git", label: "Git" },
   { icon: "devicon:github", label: "GitHub" },
-  { icon: "devicon:netlify", label: "Netlify" },
+  { icon: "devicon:gitlab", label: "GitLab" },
+  { icon: "devicon:chartjs", label: "ChartJS" },
+  { icon: "devicon:reflex", label: "Reflex" },
+  { icon: "devicon:sqlite", label: "SQLite" },
+  { icon: "devicon:postgresql", label: "PostgreSQL" },
+  { icon: "logos:chroma", label: "ChromaDB" },
+  { icon: "devicon:vscode", label: "VSCode" },
+  { icon: "logos:microsoft-power-bi", label: "Power BI" },
+  { icon: "logos:tableau-icon", label: "Tableau" },
+  { icon: "devicon:notion", label: "Notion" },
+  { icon: "devicon:markdown", label: "Markdown" },
+  { icon: "devicon:yaml", label: "YAML" },
+  { icon: "devicon:json", label: "JSON" }
 ];
+
 
 // Selects the container element where badges will be displayed
 const badgesContainer = document.getElementById("badges");
